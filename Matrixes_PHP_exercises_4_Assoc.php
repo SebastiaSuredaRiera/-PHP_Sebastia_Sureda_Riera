653946ca3827ee5eae6c3adcab2ffc19d7b6a098
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-08 18:09:59 +0100

    Matrius_EX4_Associatives_SSureda

---
+++ Matrixes_PHP_exercises_4_Assoc.php
@@ -0,0 +1,19 @@
+<?php
+
+  $FoodMenu['Breakfast']=array('Milk', 'coffee', 'toast', 'oats');
+  $FoodMenu['Lunch']=array('spanish omelet', 'spaguetti', 'beans', 'bacon');
+  $FoodMenu['Dinner']=array('soup', 'cheese', 'fruit', 'yogurt');
+
+
+foreach ($FoodMenu as $Value_1 => $Value_2 ) {
+  echo "<b> $Value_1</b>";
+  echo ":";
+  foreach ($Value_2 as $Value_3 => $Value_4)  {
+ echo " $Value_4 ";
+  }
+  echo"</br>";
+}
+
+
+
+?>
