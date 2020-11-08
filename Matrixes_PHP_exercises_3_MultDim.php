9fc69f614546b3ed2962fe71fc71a26a29178dd5
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-08 18:09:35 +0100

    Matrius_EX_3_MultiDimensionals_SSureda

--- Matrixes_PHP_exercises_3_MultDim.php
+++ Matrixes_PHP_exercises_3_MultDim.php
@@ -6,15 +6,13 @@ $FoodMenus=array(
     "Dinner" => array ( 'soup', 'cheese', 'fruit', 'yogurt')
   );
 
-
-
-foreach ($FoodMenus as $Value_1 => $Value_2 ) {
-  echo "<b>$Value_1</b>";
-  echo ":";
-  foreach ($Value_2 as $Value_3 => $Value_4)  {
- echo "$Value_4";
+  foreach ($FoodMenus as $Value_1 => $Value_2 ) {
+    echo "<b>$Value_1</b>";
+    echo ":";
+    foreach ($Value_2 as $Value_3 => $Value_4)  {
+   echo "$Value_4 ";
+    }
+    echo"</br>";
   }
-  echo"</br>";
-}
 
  ?>
