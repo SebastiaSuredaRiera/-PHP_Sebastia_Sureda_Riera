45e33315c7c05dde4a9548c40e46d5ec7c9809e4
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-08 17:40:14 +0100

    Matrixes_Multidimensionals_EX_3

---
+++ Matrixes_PHP_exercises_3_MultDim.php
@@ -0,0 +1,20 @@
+<?php
+
+$FoodMenus=array(
+    "Breakfast" => array ('Milk', 'coffee', 'toast', 'oats'),
+    "Lunch" => array ('spanish omelet', 'spaguetti', 'beans', 'bacon'),
+    "Dinner" => array ( 'soup', 'cheese', 'fruit', 'yogurt')
+  );
+
+
+
+foreach ($FoodMenus as $Value_1 => $Value_2 ) {
+  echo "<b>$Value_1</b>";
+  echo ":";
+  foreach ($Value_2 as $Value_3 => $Value_4)  {
+ echo "$Value_4";
+  }
+  echo"</br>";
+}
+
+ ?>
