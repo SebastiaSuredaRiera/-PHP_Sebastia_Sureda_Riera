6da0531d295d7ee63107afffa246c7833dd328f0
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-10 19:08:54 +0100

    Formularis

---
+++ Formularis/SeleccioMultiple.php
@@ -0,0 +1,10 @@
+<?php
+    /*foreach ($_POST['navegadors'] as $opcio)
+                echo $opcio . "\n";*/
+
+    foreach ($_GET['navegadors'] as $opcio)
+
+                echo $opcio . "\n </br>";
+                echo "$_GET[nom] </br>";
+                echo "$_GET[email] </br>";
+?>
