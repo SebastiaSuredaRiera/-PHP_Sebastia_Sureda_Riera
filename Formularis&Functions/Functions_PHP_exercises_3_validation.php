5619351099590a6838bda7900b6e2dfaa3e29963
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-26 19:36:35 +0100

    form_intermediate_2_validation.php

--- Formularis&Functions/Functions_PHP_exercises_3_validation.php
+++ Formularis&Functions/Functions_PHP_exercises_3_validation.php
@@ -1,5 +1,6 @@
 <?php
 $nom="Sebastià Sureda Riera";
+
 function fullnamesplit($nom) {
   return (explode(" ",$nom));
 }
@@ -7,4 +8,17 @@ $solucio=fullnamesplit($nom);
 foreach ($solucio as $Value_1) {
     echo "$Value_1 </br> ";
   }
+
+$pwd="12345678";
+  function password($pwd) {
+    return password_hash($pwd, PASSWORD_DEFAULT);
+  }
+
+    $sol=password($pwd); {
+        echo "$sol </br> ";
+}
+
+
+
+
  ?>
