57836782eab8e32f2cce20d0502547d6f795099a
Violeta <violetaisusi@gmail.com>
2020-11-27 20:27:11 +0100

    visusi - vectors to learn 2 - KO

--- Vectors_PHP_exercises_to_learn_2.php
+++ Vectors_PHP_exercises_to_learn_2.php
@@ -1,4 +1,6 @@
 <?php
+//visusi corrections - El vector a aquest exercici ha de ser encara el dels dies de la setmana
+//Refer-lo per a practicar
 $Months=array(
     "January" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31.),
     "February" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29.),
