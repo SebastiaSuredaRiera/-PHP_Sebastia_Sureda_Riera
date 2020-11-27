a49a84b8003a1f20b1b9509ae96be0599e4dd2d3
Violeta <violetaisusi@gmail.com>
2020-11-27 21:49:00 +0100

    visusi - Functions 2 - KO

--- Formularis&Functions/Functions_PHP_exercises_2_validation.php
+++ Formularis&Functions/Functions_PHP_exercises_2_validation.php
@@ -1,5 +1,5 @@
 <?php
-
+//visusi corrections - KO
 function  emailvalidation($email){
   return filter_var($email,FILTER_VALIDATE_EMAIL);
 }
@@ -19,12 +19,13 @@ echo "</br>";
 echo "</br>";
 
 function passwordvalidation($Val_1,$Val_2){
-if  (strcmp($Val_1,$Val_2) == 0)
-  return false;
-return True;
+if  (strcmp($Val_1,$Val_2) == 0)//visusi corrections - quan strcmp retorna 0 es que  les cadenes string comparades  son iguals
+  return false; // visusi - Ha de ser True - Corregir
+return True;// visusi - Ha de ser False- Corregir
 }
 
-echo passwordvalidation("123","123");
+echo passwordvalidation("123","123"); //visusi corrections - Com la funcio retorna True o False no se mostra cap missatge. 
+                                                              //canviar per a que se mostre un missatge. de "paswords iguals" quan la funció retorne True i "No son iguals" en cas contrari 
 
 
 
