debf33aed9a869a66e795eddbd9cc01d2e877d3f
Violeta <violetaisusi@gmail.com>
2020-11-27 21:09:03 +0100

    visusi - Matrixes MultiD 1 - KO

--- Matrixes_PHP_exercises_1.php
+++ Matrixes_PHP_exercises_1.php
@@ -1,5 +1,12 @@
 <?php
-
+/*visusi - corrections KO
+falta emprar variables per a emmgatzemar els dies de la setmana i no repetir tant de codi igual.
+exemple: 
+$days_31 = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31);
+$Months=array(
+    "January" =>$days_31,
+    .........
+*/
 $Months=array(
     "January" => array (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31),
     "February" => array (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29),
