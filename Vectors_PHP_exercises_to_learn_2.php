4ef8f6ea95760c3060ab327404d2e7521d9eb5b7
SebastiaSuredaRiera <moraryt@gmail.com>
2020-11-28 21:40:14 +0100

    corregit

--- Vectors_PHP_exercises_to_learn_2.php
+++ Vectors_PHP_exercises_to_learn_2.php
@@ -16,8 +16,12 @@ $Months=array(
     "Dicember" => array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31.)
   );
 
-foreach ($Months as $Month => $Days)
-{
-  echo "$Month: $Days <br>";
+
+foreach ($Months as $Month => $Days){
+  echo "$Month<br>";
+  foreach ($Days as $valor3 => $valor4){
+    echo "$valor4 ";
+  }
+  echo "</br></br>";
 }
 ?>
